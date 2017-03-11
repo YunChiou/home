78c48bc9c318b9ea2e1c6798639f3bb4337543da
ChenYiChien <elaine85060311@gmail.com>
2017-03-11 20:53:20 +0800

    每次進入頁面都畫出現有的所有桌子

--- app/src/main/java/com/example/asus/home/PaintBoard.java
+++ app/src/main/java/com/example/asus/home/PaintBoard.java
@@ -32,6 +32,7 @@ public class PaintBoard extends View {
 
     public PaintBoard(TableLayout tableLayout) {
         super(tableLayout);
+        new GetAllTables(allTables).execute();
         this.tableLayout = tableLayout;
         paint = new Paint();
         int tableColor = tableLayout.getResources().getColor(R.color.yello);
