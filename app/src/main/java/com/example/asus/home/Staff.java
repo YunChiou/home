b9792e67aa4b28d84af1fa741bc07b9d7c5ee14a
unknown <atiffany192130@gmail.com>
2017-03-18 19:50:31 +0800

    增加staff 裡的restaurantPhone

--- app/src/main/java/com/example/asus/home/Staff.java
+++ app/src/main/java/com/example/asus/home/Staff.java
@@ -5,14 +5,17 @@ public class Staff extends User{
     private String staffID;
     private String restaurantName;
     private String restaurantAdd;
+    private String restaurantPhone;
 
-    public Staff(String account, String password, String name, String staffID, String restaurantName, String restaurantAdd) {
+    public Staff(String account, String password, String name, String staffID, String restaurantName, String restaurantAdd , String restaurantPhone) {
         super.account = account;
         super.password = password;
         super.name = name;
         this.staffID = staffID;
         this.restaurantName = restaurantName;
         this.restaurantAdd = restaurantAdd;
+        this.restaurantPhone = restaurantPhone;
+
     }
 
     public String getStaffID() {
@@ -26,5 +29,7 @@ public class Staff extends User{
     public String getrestaurantAdd() {
         return restaurantAdd;
     }
+
+    public String getRestaurantPhone(){return restaurantPhone;}
 
 }
