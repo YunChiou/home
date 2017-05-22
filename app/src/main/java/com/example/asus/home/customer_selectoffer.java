dcc220c3bd4d089689186489f85b4e83039881d6
diefishlike <diefishlike@gmail.com>
2017-05-23 00:31:39 +0800

    ICON選擇喔

---
+++ app/src/main/java/com/example/asus/home/customer_selectoffer.java
@@ -0,0 +1,13 @@
+package com.example.asus.home;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class customer_selectoffer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_selectoffer);
+    }
+}
