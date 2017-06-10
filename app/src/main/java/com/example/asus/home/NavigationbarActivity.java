316ef15184d0f224a4c758c138fe8ff2d59bd47a
diefishlike <diefishlike@gmail.com>
2017-06-10 20:12:34 +0800

    no message

--- app/src/main/java/com/example/asus/home/NavigationbarActivity.java
+++ app/src/main/java/com/example/asus/home/NavigationbarActivity.java
@@ -108,6 +108,11 @@ public class NavigationbarActivity extends AppCompatActivity implements Navigati
             intent.setClass(NavigationbarActivity.this, All_Restaurants.class);
             startActivity(intent);
         }
+        else if(id == R.id.ten){
+            Intent intent = new Intent();
+            intent.setClass(NavigationbarActivity.this, customer_selectoffer.class);
+            startActivity(intent);
+        }
 
         else if(id == R.id.logout){
             Intent intent = new Intent();
