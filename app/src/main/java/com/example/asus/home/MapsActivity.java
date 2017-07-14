5180eef4d237a04330f49b25019a4cb4d571a935
unknown <atiffany192130@gmail.com>
2017-07-14 13:09:11 +0800

    改些東西

--- app/src/main/java/com/example/asus/home/MapsActivity.java
+++ app/src/main/java/com/example/asus/home/MapsActivity.java
@@ -10,7 +10,7 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends ToolbarActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
 
@@ -19,6 +19,8 @@ public class MapsActivity extends ToolbarActivity implements OnMapReadyCallback 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+
+
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
