a956be3c916c64f41d9159a789737a4cd9aa65ff
ChenYiChien <elaine85060311@gmail.com>
2017-07-20 04:07:31 +0800

    地圖改到主頁

--- app/src/main/java/com/example/asus/home/HomePage.java
+++ app/src/main/java/com/example/asus/home/HomePage.java
@@ -15,10 +15,33 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.*;
 
-public class HomePage extends Nav_drawer {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class HomePage extends Nav_drawer implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         super.addContentView(R.layout.activity_home_page);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(23, 120);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("餐廳A"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
