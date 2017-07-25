8a9f499bd842cdf4ba224f365440a309535cc096
unknown <atiffany192130@gmail.com>
2017-07-25 19:50:38 +0800

    標記餐廳

--- app/src/main/java/com/example/asus/home/HomePage.java
+++ app/src/main/java/com/example/asus/home/HomePage.java
@@ -19,6 +19,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -39,16 +40,37 @@ public class HomePage extends Nav_drawer implements OnMapReadyCallback {
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(23, 120);
         mMap.addMarker(new MarkerOptions().position(sydney).title("餐廳A"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
 
+        MarkerOptions one = new MarkerOptions();
+        one.position(new LatLng(25.034871, 121.507356));
+        one.title("餐廳B");
+        one.snippet("每周二公休");
+        one.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE));
+        one.draggable(false);
+        one.visible(true);
+        mMap.addMarker(one);
+
+        MarkerOptions two = new MarkerOptions();
+        two.position(new LatLng(25.003396, 121.513303));
+        two.title("餐廳C");
+        two.snippet("假日營業時間為整天");
+        two.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN));
+        two.draggable(false);
+        two.visible(true);
+        mMap.addMarker(two);
+
         //mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
         //mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
-        //mMap.setMyLocationEnabled(true);
+        mMap.setMyLocationEnabled(true);
         mMap.getUiSettings().setZoomControlsEnabled(true); // 右下角的放大縮小功能
         mMap.getUiSettings().setCompassEnabled(true); // 左上角的指南針，要兩指旋轉才會出現
         mMap.getUiSettings().setMapToolbarEnabled(true); // 右下角的導覽及開啟 Google Map功能
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(5));
     }
 }
