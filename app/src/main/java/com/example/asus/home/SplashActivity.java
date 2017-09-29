1ccdced5666fde74b459b01a2ef6df47e60d54d7
ChenYiChien <elaine85060311@gmail.com>
2017-09-29 22:09:34 +0800

    加logo

---
+++ app/src/main/java/com/example/asus/home/SplashActivity.java
@@ -0,0 +1,47 @@
+package com.example.asus.home;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static String TAG = SplashActivity.class.getName();
+    private static long SLEEP_TIME = 1;    // Time in seconds to show the picture
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);    // Removes title bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);    // Removes notification bar
+
+        setContentView(R.layout.activity_splash);  //your layout with the picture
+
+        // Start timer and launch main activity
+        IntentLauncher launcher = new IntentLauncher();
+        launcher.start();
+    }
+
+    private class IntentLauncher extends Thread {
+        @Override
+        /**
+         * Sleep for some time and than start new activity.
+         */
+        public void run() {
+            try {
+                // Sleeping
+                Thread.sleep(SLEEP_TIME * 1800);
+            } catch (Exception e) {
+
+            }
+
+            // Start main activity
+            Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+            SplashActivity.this.startActivity(intent);
+            SplashActivity.this.finish();
+        }
+    }
+}
